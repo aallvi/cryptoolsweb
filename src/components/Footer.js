6056c8a01cc7364c090af60caad159f5d6394e47
Alvaro <aleiva97@gmail.com>
2022-04-11 17:27:05 -0400

    Terminado falta estilizar mejor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './footer.css'
+import googleplay from '../img/googleplay.png'
+
+
 
 export const Footer = () => {
 
@@ -10,11 +13,13 @@ export const Footer = () => {
     <div className='container footer' >
 
       
-         <p className='texto' > Descarga la APP para Android <a className='link' target='_blank' href='https://play.google.com/store/apps/details?id=com.criptotoolsalvi&hl=es_CL&gl=US' > Aqui  </a> </p>
+         <p className='texto' > Descarga la APP para Android  </p>
+
+         <a className='link' target='_blank' href='https://play.google.com/store/apps/details?id=com.criptotoolsalvi&hl=es_CL&gl=US' > <img src={googleplay} width={170} />  </a> 
             
             <div className='contenedorCopy' >
                   <p className='textoCopyright' > Creado por Alvaro Leiva </p>
-                  <a href='https://www.linkedin.com/in/alvaro-leiva-gil-aaa6ba201/' target='_blank' >  <img src='https://cdn-icons.flaticon.com/png/512/3536/premium/3536569.png?token=exp=1646798840~hmac=c1a6486ff96a97304f6fc09311875960' width='100' height='200px' /></a>
+                  <a href='https://www.linkedin.com/in/alvaro-leiva-gil-aaa6ba201/' target='_blank' >  <img src='https://cdn-icons.flaticon.com/png/512/3536/premium/3536569.png?token=exp=1649713272~hmac=521f8021431051036cff7212c4442308' width='100' height='200px' /></a>
             </div>
 
 
