db66ed950e7cb105a9cce2315ad9ff664f841ad9
Alvaro <aleiva97@gmail.com>
2022-04-12 12:29:04 -0400

    precios cripto listo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,163 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 export const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+
+
+
+
   return (
     <nav>
-    
-    <ul>
-        <li>
+
+
+      {toggleMenu &&
+
+            <ul>
+            <li>
+                <NavLink
+                to="precio-criptomonedas"
+                className={({ isActive }) =>
+                  isActive ? 'activeClassName' : 'BotonNav'
+                }
+              >
+                Precios Criptomonedas
+              </NavLink>
+              
+              </li>
+
+
+            <li>
+
             <NavLink
-            to="precio-criptomonedas"
-            className={({ isActive }) =>
-              isActive ? 'activeClassName' : 'BotonNav'
-            }
-          >
-            Precios Criptomonedas
-          </NavLink>
-          
-          </li>
+                to="precio-criptomonedas-historico"
+                className={({ isActive }) =>
+                  isActive ? 'activeClassName' : 'BotonNav'
+                }
+              >
+                Precios Historicos Criptomonedas
+              </NavLink>
 
-
-        <li>
-
-        <NavLink
-            to="precio-criptomonedas-historico"
-            className={({ isActive }) =>
-              isActive ? 'activeClassName' : 'BotonNav'
-            }
-          >
-            Precios Historicos Criptomonedas
-          </NavLink>
-
-        </li>
+            </li>
 
 
 
 
-        <li>
+            <li>
 
-        <NavLink
-            to="calculadora"
-            className={({ isActive }) =>
-              isActive ? 'activeClassName' : 'BotonNav'
-            }
-          >
-            Calculadora
-          </NavLink>
-
-
-        </li>
+            <NavLink
+                to="calculadora"
+                className={({ isActive }) =>
+                  isActive ? 'activeClassName' : 'BotonNav'
+                }
+              >
+                Calculadora
+              </NavLink>
 
 
-        <li>
-         
-        <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? 'activeClassName' : 'BotonNav'
-            }
-          >
-            Impermanent Loss
-          </NavLink>
+            </li>
 
-        </li>
+
+            <li>
+            
+            <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? 'activeClassName' : 'BotonNav'
+                }
+              >
+                Impermanent Loss
+              </NavLink>
+
+            </li>
+
+
+
+            </ul>
+                  
+      
+      
+      }
+
+      {screenWidth > 770 &&
+      
+      
+      <ul>
+      <li>
+          <NavLink
+          to="precio-criptomonedas"
+          className={({ isActive }) =>
+            isActive ? 'activeClassName' : 'BotonNav'
+          }
+        >
+          Precios Criptomonedas
+        </NavLink>
         
+        </li>
 
 
-    </ul>
+      <li>
+
+      <NavLink
+          to="precio-criptomonedas-historico"
+          className={({ isActive }) =>
+            isActive ? 'activeClassName' : 'BotonNav'
+          }
+        >
+          Precios Historicos Criptomonedas
+        </NavLink>
+
+      </li>
 
 
+
+
+      <li>
+
+      <NavLink
+          to="calculadora"
+          className={({ isActive }) =>
+            isActive ? 'activeClassName' : 'BotonNav'
+          }
+        >
+          Calculadora
+        </NavLink>
+
+
+      </li>
+
+
+      <li>
+      
+      <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'activeClassName' : 'BotonNav'
+          }
+        >
+          Impermanent Loss
+        </NavLink>
+
+      </li>
+
+
+
+      </ul>
+      
+      
+      
+      }
+    
+    
+
+    <button className="icon" onClick={() => setToggleMenu(!toggleMenu) }  >
+        <i className="fa fa-bars"></i>
+      </button>
 
 
     </nav>
